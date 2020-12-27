982b47091400d85376220f1a26e89c0d7c835dd7
zowan <zowan@zowanui-MacBookPro.local>
2020-12-27 22:48:53 +0900

    done: 기본적인 라우팅과 레이아웃 정의

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
 import './App.css';
 
 function App() {
   return (
-    <div>
+    <>
+      <head>
+        <meta name="description" content="세상 모든 테크트리, tect.dev" />
+      </head>
       <header></header>
-    </div>
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+    </>
   );
 }
 
---
+++ src/lib/dummyTechtree.js
@@ -0,0 +1,558 @@
+export const dummyTechtree = {"physics":{"nodes":[
+    {
+      "id": 0,
+      "name": "선형대수",
+      "label": "선형대수",
+      "group": 2,
+      "recentMileage": [1,2,4,5,6]
+    },
+    {
+      "id": 1,
+      "name": "미적분학과벡터해석",
+      "label": "미적분",
+      "group": 1,
+      "recentMileage": [1,2,4,5,6]
+    },
+    {
+      "id": 2,
+      "name": "고급미적분학",
+      "label": "고미1",
+      "group": 2,
+      "recentMileage": [1,2,4,5,6]
+    },
+    {
+      "id": 3,
+      "name": "현대물리",
+      "label": "현대물리",
+      "group": 2,
+      "recentMileage": [1,2,4,5,6]
+    },
+    {
+      "id": 4,
+      "name": "고전역학",
+      "label": "고전역학",
+      "group": 2,
+      "recentMileage": [1,2,4,5,6]
+    },
+    {
+      "id": 5,
+      "name": "열물리",
+      "label": "열물리",
+      "group": 3,
+      "recentMileage": [1,2,4,5,6]
+    },
+    {
+      "id": 6,
+      "name": "통계물리",
+      "label": "통계물리",
+      "group": 3,
+      "recentMileage": [1,2,4,5,6]
+    },
+    {
+      "id": 7,
+      "name": "양자역학",
+      "label": "양자역학",
+      "group": 3,
+      "recentMileage": [1,2,4,5,6]
+    },
+    {
+      "id": 8,
+      "name": "전자기학",
+      "label": "전자기학",
+      "group": 3,
+      "recentMileage": [1,2,4,5,6]
+    },
+    {
+      "id": 9,
+      "name": "군론",
+      "label": "군론",
+      "group": 3,
+      "recentMileage": [1,2,4,5,6]
+    },
+    {
+      "id": 10,
+      "name": "입자물리",
+      "label": "입자물리",
+      "group": 4,
+      "recentMileage": [1,2,4,5,6]
+    },
+    {
+      "id": 11,
+      "name": "핵물리",
+      "label": "핵물리",
+      "group": 4,
+      "recentMileage": [1,2,4,5,6]
+    },
+    {
+      "id": 12,
+      "name": "상대론",
+      "label": "상대론",
+      "group": 4,
+      "recentMileage": [1,2,4,5,6]
+    },
+    {
+      "id": 13,
+      "name": "광학",
+      "label": "광학",
+      "group": 4,
+      "recentMileage": [1,2,4,5,6]
+    },
+    {
+      "id": 14,
+      "name": "고체물리",
+      "label": "고체물리",
+      "group": 4,
+      "recentMileage": [1,2,4,5,6]
+    },
+    {
+      "id": 15,
+      "name": "A1",
+      "label": "A1",
+      "group": 4,
+      "recentMileage": [1,2,4,5,6]
+    },
+    {
+      "id": 16,
+      "name": "B1",
+      "label": "B1",
+      "group": 3,
+      "recentMileage": [1,2,4,5,6]
+    }
+  ],"links":[
+    {
+      "source": 0,
+      "target": 7,
+      "type": ""
+    },
+    {
+      "source": 0,
+      "target": 9,
+      "type": ""
+    },
+    {
+      "source": 1,
+      "target": 2,
+      "type": ""
+    },
+    {
+      "source": 1,
+      "target": 0,
+      "type": ""
+    },
+    {
+      "source": 2,
+      "target": 4,
+      "type": ""
+    },
+    {
+      "source": 2,
+      "target": 8,
+      "type": ""
+    },
+    {
+      "source": 3,
+      "target": 7,
+      "type": ""
+    },
+    {
+      "source": 3,
+      "target": 12,
+      "type": ""
+    },
+    {
+      "source": 4,
+      "target": 7,
+      "type": ""
+    },
+    {
+      "source": 5,
+      "target": 6,
+      "type": ""
+    },
+    {
+      "source": 6,
+      "target": 14,
+      "type": ""
+    },
+    {
+      "source": 7,
+      "target": 11,
+      "type": ""
+    },
+    {
+      "source": 7,
+      "target": 12,
+      "type": ""
+    },
+    {
+      "source": 7,
+      "target": 10,
+      "type": ""
+    },
+    {
+      "source": 7,
+      "target": 13,
+      "type": ""
+    },
+    {
+      "source": 7,
+      "target": 14,
+      "type": ""
+    },
+    {
+      "source": 8,
+      "target": 12,
+      "type": ""
+    },
+    {
+      "source": 8,
+      "target": 13,
+      "type": ""
+    },
+    {
+      "source": 8,
+      "target": 14,
+      "type": ""
+    },
+    {
+      "source": 9,
+      "target": 10,
+      "type": ""
+    },
+    {
+      "source": 9,
+      "target": 11,
+      "type": ""
+    },
+    {
+      "source": 9,
+      "target": 12,
+      "type": ""
+    }
+  ]},
+      "cs":{
+      "nodes": [
+        {
+          "id": 0,
+          "name": "선형대수",
+          "label": "선형대수",
+          "group": 2,
+          "recentMileage": [1,2,4,5,6]
+        },
+        {
+          "id": 1,
+          "name": "공학수학",
+          "label": "공학수학",
+          "group": 1,
+          "recentMileage": [1,2,4,5,6]
+        },
+        {
+          "id": 2,
+          "name": "확률통계",
+          "label": "확률통계",
+          "group": 1,
+          "recentMileage": [1,2,4,5,6]
+        },
+        {
+          "id": 3,
+          "name": "컴퓨터그래픽스",
+          "label": "컴그",
+          "group": 4,
+          "recentMileage": [1,2,4,5,6]
+        },
+        {
+          "id": 4,
+          "name": "이산수학",
+          "label": "이산수학",
+          "group": 2,
+          "recentMileage": [1,2,4,5,6]
+        },
+        {
+          "id": 5,
+          "name": "객체지향프로그래밍",
+          "label": "OOP",
+          "group": 1,
+          "recentMileage": [1,2,4,5,6]
+        },
+        {
+          "id": 6,
+          "name": "자료구조",
+          "label": "자료구조",
+          "group": 2,
+          "recentMileage": [1,2,4,5,6]
+        },
+        {
+          "id": 7,
+          "name": "컴퓨터프로그래밍",
+          "label": "컴프밍",
+          "group": 2,
+          "recentMileage": [1,2,4,5,6]
+        },
+        {
+          "id": 8,
+          "name": "프로그래밍언어구조",
+          "label": "프언구",
+          "group": 2,
+          "recentMileage": [1,2,4,5,6]
+        },
+        {
+          "id": 9,
+          "name": "컴파일러",
+          "label": "컴파일러",
+          "group": 3,
+          "recentMileage": [1,2,4,5,6]
+        },
+        {
+          "id": 10,
+          "name": "오토마타",
+          "label": "오토마타",
+          "group": 3,
+          "recentMileage": [1,2,4,5,6]
+        },
+        {
+          "id": 11,
+          "name": "논리회로설계",
+          "label": "논회설",
+          "group": 2,
+          "recentMileage": [1,2,4,5,6]
+        },
+        {
+          "id": 12,
+          "name": "알고리즘",
+          "label": "알고리즘",
+          "group": 2,
+          "recentMileage": [1,2,4,5,6]
+        },
+        {
+          "id": 13,
+          "name": "HCI",
+          "label": "HCI",
+          "group": 2,
+          "recentMileage": [1,2,4,5,6]
+        },
+        {
+          "id": 14,
+          "name": "인공지능",
+          "label": "인공지능",
+          "group": 2,
+          "recentMileage": [1,2,4,5,6]
+        },
+        {
+          "id": 15,
+          "name": "데이터마이닝",
+          "label": "데마",
+          "group": 3,
+          "recentMileage": [1,2,4,5,6]
+        },
+        {
+          "id": 16,
+          "name": "데이터베이스",
+          "label": "DB",
+          "group": 2,
+          "recentMileage": [1,2,4,5,6]
+        },
+        {
+          "id": 17,
+          "name": "네트워크",
+          "label": "네트워크",
+          "group": 2,
+          "recentMileage": [1,2,4,5,6]
+        },
+        {
+          "id": 18,
+          "name": "인터넷프로그래밍",
+          "label": "인프밍",
+          "group": 2,
+          "recentMileage": [1,2,4,5,6]
+        },
+        {
+          "id": 19,
+          "name": "컴퓨터시스템",
+          "label": "컴시",
+          "group": 2,
+          "recentMileage": [1,2,4,5,6]
+        },
+        {
+          "id": 20,
+          "name": "운영체제",
+          "label": "OS",
+          "group": 2,
+          "recentMileage": [1,2,4,5,6]
+        },
+        {
+          "id": 21,
+          "name": "시스템프로그래밍",
+          "label": "시프밍",
+          "group": 4,
+          "recentMileage": [1,2,4,5,6]
+        },
+        {
+          "id": 22,
+          "name": "멀티코어",
+          "label": "멀티코어",
+          "group": 4,
+          "recentMileage": [1,2,4,5,6]
+        },
+        {
+          "id": 23,
+          "name": "컴퓨터비전",
+          "label": "컴비전",
+          "group": 4,
+          "recentMileage": [1,2,4,5,6]
+        },
+        {
+          "id": 24,
+          "name": "머신러닝",
+          "label": "머신러닝",
+          "group": 4,
+          "recentMileage": [1,2,4,5,6]
+        },
+        {
+          "id": 25,
+          "name": "아키텍쳐",
+          "label": "아키텍쳐",
+          "group": 4,
+          "recentMileage": [1,2,4,5,6]
+        },
+        {
+          "id": 26,
+          "name": "정보보호",
+          "label": "정보보호",
+          "group": 4,
+          "recentMileage": [1,2,4,5,6]
+        }
+      ],
+      "links": [
+        {
+          "source": 0,
+          "target": 24,
+          "type": "동시수강 가능?"
+        },
+        {
+          "source": 0,
+          "target": 23,
+          "type": ""
+        },
+        {
+          "source": 0,
+          "target": 3,
+          "type": "동시수강 가능??"
+        },
+        {
+          "source": 2,
+          "target": 23,
+          "type": ""
+        },
+        {
+          "source": 2,
+          "target": 14,
+          "type": ""
+        },
+        {
+          "source": 6,
+          "target": 24,
+          "type": ""
+        },
+        {
+          "source": 6,
+          "target": 14,
+          "type": ""
+        },
+        {
+          "source": 6,
+          "target": 12,
+          "type": ""
+        },
+        {
+          "source": 6,
+          "target": 11,
+          "type": ""
+        },
+        {
+          "source": 6,
+          "target": 10,
+          "type": ""
+        },
+        {
+          "source": 6,
+          "target": 15,
+          "type": ""
+        },
+        {
+          "source": 6,
+          "target": 20,
+          "type": ""
+        },
+        {
+          "source": 6,
+          "target": 19,
+          "type": ""
+        },
+        {
+          "source": 5,
+          "target": 6,
+          "type": ""
+        },
+        {
+          "source": 1,
+          "target": 23,
+          "type": ""
+        },
+        {
+          "source": 7,
+          "target": 5,
+          "type": ""
+        },
+        {
+          "source": 4,
+          "target": 10,
+          "type": ""
+        },
+        {
+          "source": 4,
+          "target": 11,
+          "type": ""
+        },
+        {
+          "source": 4,
+          "target": 12,
+          "type": ""
+        },
+        {
+          "source": 17,
+          "target": 26,
+          "type": ""
+        },
+        {
+          "source": 19,
+          "target": 26,
+          "type": ""
+        },
+        {
+          "source": 18,
+          "target": 16,
+          "type": ""
+        },
+        {
+          "source": 16,
+          "target": 26,
+          "type": ""
+        },
+        {
+          "source": 19,
+          "target": 25,
+          "type": ""
+        },
+        {
+          "source": 25,
+          "target": 22,
+          "type": ""
+        },
+        {
+          "source": 20,
+          "target": 21,
+          "type": ""
+        }
+      ]
+    }
+    }    
