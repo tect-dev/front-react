5a9df5b0607930151e12331fc3a55047bec37a99
zowan <zowan@zowanui-MacBookPro.local>
2021-01-03 21:43:52 +0900

    done: markdown 에디터 및 수식입력 프리뷰 적용. todo: 텍스트입력창에서 엔터입력 인식

--- src/components/question/QuestionWriteBlock.js
+++ src/components/question/QuestionWriteBlock.js
@@ -1,8 +1,12 @@
-import React, { useCallback } from 'react';
-
+import React, { useCallback, useState } from 'react';
+import { uid } from 'uid';
 import unified from 'unified';
 import parse from 'remark-parse';
 import remark2react from 'remark-react';
+import math from 'remark-math';
+import remark2rehype from 'remark-rehype';
+import katex from 'rehype-katex';
+import stringify from 'rehype-stringify';
 
 import { useInput } from '../../hooks/hooks';
 import { useSelector, useDispatch } from 'react-redux';
@@ -13,7 +17,25 @@ export default function QuestionWriteBlock() {
     return { loginState: state.auth.loginState, userInfo: state.auth.userInfo };
   });
   const [title, onChangeTitle] = useInput();
-  const [content, onChangeContent] = useInput();
+  //const [content, onChangeContent] = useInput('$\\frac{v^{2}}{2}$');
+  const [content, setContent] = useState();
+
+  function onChangeContent(e) {
+    setContent(e.target.value);
+    setHtml(
+      unified()
+        //.use(breaks)
+        .use(parse)
+        //.use(slug)
+        .use(remark2rehype)
+        //.use(raw)
+        .use(math)
+        .use(katex)
+        .use(stringify)
+        .processSync(e.target.value)
+        .toString()
+    );
+  }
 
   const dispatch = useDispatch();
 
@@ -22,9 +44,12 @@ export default function QuestionWriteBlock() {
       return alert('게시글을 작성하세요.');
     }
     const formData = new FormData();
+    const uid24 = uid(24);
+    formData.append('postUID', uid24);
     formData.append('contentType', 'question');
     formData.append('title', title);
     formData.append('content', content);
+
     if (userInfo.userUID) {
       formData.append('authorUID', userInfo.userUID);
     } else {
@@ -32,6 +57,24 @@ export default function QuestionWriteBlock() {
     }
     dispatch(createQuestion(formData));
   }, [title, content]);
+
+  const [html, setHtml] = useState(
+    unified()
+      //.use(breaks)
+      .use(parse)
+      //.use(slug)
+      //.use(prismPlugin)
+      //.use(embedPlugin)
+      .use(remark2rehype)
+      //.use(raw)
+      .use(math)
+      .use(katex)
+      .use(stringify)
+      //.use(remark2react)
+      .processSync(content)
+      //.result
+      .toString()
+  );
 
   return (
     <>
@@ -55,7 +98,16 @@ export default function QuestionWriteBlock() {
           ></textarea>
         </div>
         <div id="preview">
-          {unified().use(parse).use(remark2react).processSync(content).result}
+          <div dangerouslySetInnerHTML={{ __html: html }} />
+          {
+            //unified()
+            //  .use(parse)
+            //  .use(math)
+            //  .use(remark2rehype)
+            //  .use(katex)
+            //  .use(remark2react)
+            //  .processSync(content).result
+          }
         </div>
         <div class="button">
           <button type="submit">Send your message</button>
