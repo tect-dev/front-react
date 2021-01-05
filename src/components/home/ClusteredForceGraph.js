ae1e4df3dbb76c550fc88f51a35af0e17ecf1464
zowan <zowan@zowanui-MacBookPro.local>
2021-01-06 01:12:02 +0900

    done: 정외과 추가. todo: 곡선 화살표 그리기

---
+++ src/components/home/ClusteredForceGraph.js
@@ -0,0 +1,200 @@
+import React from 'react'
+import * as d3 from 'd3'
+import { forceAttract } from 'd3-force-attract'
+import { forceCluster } from 'd3-force-cluster'
+import styles from '../../styles/Techtree.module.css'
+
+export default function ClusteredForceGraph({ techtreeData, category }) {
+  const containerRef = React.useRef(null)
+
+  const nodeHoverTooltip = (node) => {
+    return `<div>     
+      <p><b>${node.name}</b></p>
+      <p>최근 5년<br />마일리지 커트라인<br />${node.recentMileage}</p>
+    </div>`
+  }
+
+  React.useEffect(() => {
+    let destroyFn
+
+    if (containerRef.current) {
+      const { destroy } = runForceGraph(
+        containerRef.current,
+        techtreeData,
+        category,
+        nodeHoverTooltip
+      )
+      destroyFn = destroy
+    }
+
+    return destroyFn
+  }, [])
+
+  return <div ref={containerRef} className={styles.container} />
+}
+
+function runForceGraph(container, techtreeData, category, nodeHoverTooltip) {
+  //const links = techtreeData.links.map((d) => Object.assign({}, d));
+  const m = 4 // number of distinct clusters
+  const maxRadius = 12
+  let clusters = new Array(m)
+  const nodes = techtreeData.nodes.map((d) => {
+    const i = Math.floor(Math.random() * m) // = 0~3
+    const r = Math.sqrt(((i + 1) / m) * -Math.log(Math.random())) * maxRadius
+
+    const newD = {
+      ...d,
+      cluster: d.group,
+      radius: r,
+      x:
+        Math.cos((Math.floor(Math.random() * 4) / 4) * 2 * Math.PI) * 200 +
+        width / 2 +
+        Math.random(),
+      y:
+        Math.sin((Math.floor(Math.random() * 4) / 4) * 2 * Math.PI) * 200 +
+        height / 2 +
+        Math.random(),
+    }
+    if (!clusters[i] || r > clusters[i].radius) clusters[i] = newD
+    return newD
+  })
+
+  var width = 960,
+    height = 500,
+    padding = 1.5, // separation between same-color nodes
+    clusterPadding = 6 // separation between different-color nodes
+
+  var n = 200 // total number of nodes
+
+  var color = d3.scaleSequential(d3.interpolateRainbow).domain(d3.range(m))
+
+  // The largest node for each cluster.
+
+  //var nodes = d3.range(n).map(function () {
+  //  var i = Math.floor(Math.random() * m), // = 0~3
+  //    r = Math.sqrt(((i + 1) / m) * -Math.log(Math.random())) * maxRadius,
+  //    d = {
+  //      cluster: i,
+  //      radius: r,
+  //      x: Math.cos((i / m) * 2 * Math.PI) * 200 + width / 2 + Math.random(),
+  //      y: Math.sin((i / m) * 2 * Math.PI) * 200 + height / 2 + Math.random(),
+  //    }
+  //  if (!clusters[i] || r > clusters[i].radius) clusters[i] = d
+  //  return d
+  //})
+
+  var simulation = d3
+    .forceSimulation()
+    // keep entire simulation balanced around screen center
+    .force('center', d3.forceCenter(width / 2, height / 2))
+
+    // pull toward center
+    .force(
+      'attract',
+
+      forceAttract()
+        .target([width / 2, height / 2])
+        .strength(0.01)
+    )
+
+    // cluster by section
+    .force(
+      'cluster',
+      forceCluster()
+        .centers(function (d) {
+          return clusters[d.cluster]
+        })
+        .strength(0.5)
+        .centerInertia(0.1)
+    )
+
+    // apply collision with padding
+    .force(
+      'collide',
+      d3
+        .forceCollide(function (d) {
+          return d.radius + padding
+        })
+        .strength(0)
+    )
+
+    .on('tick', layoutTick)
+    .nodes(nodes)
+
+  var svg = d3.select(container).append('svg').attr('width', width).attr('height', height)
+
+  var node = svg
+    .selectAll('circle')
+    .data(nodes)
+    .enter()
+    .append('circle')
+    .style('fill', function (d) {
+      return color(d.cluster / 10)
+    })
+    .call(d3.drag().on('start', dragstarted).on('drag', dragged).on('end', dragended))
+
+  const label = svg
+    .append('g')
+    .attr('class', 'labels')
+    .selectAll('text')
+    .data(nodes)
+    .enter()
+    .append('text')
+    .attr('text-anchor', 'middle')
+    .attr('dominant-baseline', 'central')
+    .attr('class', (d) => {
+      return d.id
+    })
+    .text((d) => {
+      return d.label
+    })
+    .style('font-weight', 'bold')
+    .style('fill', '#fff')
+    .call(d3.drag().on('start', dragstarted).on('drag', dragged).on('end', dragended))
+
+  function dragstarted(d) {
+    if (!d3.event.active) simulation.alphaTarget(0.3).restart()
+    d.fx = d.x
+    d.fy = d.y
+  }
+
+  function dragged(d) {
+    d.fx = d3.event.x
+    d.fy = d3.event.y
+  }
+
+  function dragended(d) {
+    if (!d3.event.active) simulation.alphaTarget(0)
+    d.fx = null
+    d.fy = null
+  }
+
+  // ramp up collision strength to provide smooth transition
+  var transitionTime = 3000
+  var t = d3.timer(function (elapsed) {
+    var dt = elapsed / transitionTime
+    simulation.force('collide').strength(Math.pow(dt, 2) * 0.7)
+    if (dt >= 1.0) t.stop()
+  })
+
+  function layoutTick(e) {
+    node
+      .attr('cx', function (d) {
+        return d.x
+      })
+      .attr('cy', function (d) {
+        return d.y
+      })
+      .attr('r', function (d) {
+        return d.radius
+      })
+  }
+  return {
+    destroy: () => {
+      //  simulation.stop();
+    },
+    nodes: () => {
+      return svg.node()
+    },
+  }
+}
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,39 +6,61 @@ import ForceGraph from '../components/home/ForceGraph'
 import OrbitGraph from '../components/home/OrbitGraph'
 import SubjectBlock from '../components/home/SubjectBlock'
 import { dummyTechtree } from '../lib/dummyTechtree'
+import ClusteredForceGraph from '../components/home/ClusteredForceGraph'
 
 export default function HomePage() {
   const [onPhysics, setOnPhysics] = useState(true)
+  const [selected, setSelected] = useState('physics')
+
+  const TechtreeGraph = ({ subject }) => {
+    switch (subject) {
+      case 'physics':
+        return <OrbitGraph techtreeData={dummyTechtree.physics} category={'physics'} />
+      case 'cs':
+        return <ForceGraph techtreeData={dummyTechtree.cs} category={'cs'} />
+      case 'politics':
+        return <ClusteredForceGraph techtreeData={dummyTechtree.politics} category={'politics'} />
+    }
+  }
   return (
     <>
       <MainLayout>
         <div className="container">
           <main>
-            {onPhysics ? (
-              <OrbitGraph techtreeData={dummyTechtree.physics} category={'physics'} />
-            ) : (
-              <ForceGraph techtreeData={dummyTechtree.cs} category={'cs'} />
-            )}
+            <TechtreeGraph subject={selected} />
           </main>
           <aside className="sidebar">
             <div
               onClick={() => {
                 console.log('physics clicked')
-                setOnPhysics(true)
+                //setOnPhysics(true)
+                setSelected('physics')
               }}
               className="block"
             >
-              <SubjectBlock iconSize="70" url="physics" displayedName="physics" />
+              <SubjectBlock iconSize="70" url="physics" displayedName="물리학" />
             </div>
 
             <div
               onClick={() => {
                 console.log('cs clicked')
-                setOnPhysics(false)
+                //setOnPhysics(true)
+                setSelected('cs')
               }}
               className="block"
             >
-              <SubjectBlock iconSize="50" url="computer" displayedName="Computer Science" />
+              <SubjectBlock iconSize="50" url="computer" displayedName="컴퓨터과학" />
+            </div>
+
+            <div
+              onClick={() => {
+                console.log('cs clicked')
+                //   setOnPhysics(false)
+                setSelected('politics')
+              }}
+              className="block"
+            >
+              <SubjectBlock iconSize="50" url="computer" displayedName="정치외교학" />
             </div>
 
             <div onClick={() => {}} className="block">
