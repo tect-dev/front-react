8bfa29d2ce5378c81714c830edb7cb3132caf685
zowan <zowan@zowanui-MacBookPro.local>
2021-01-05 17:01:57 +0900

    done: 연관 과목만 보여주기 구현. todo: 화살표

--- src/components/home/OrbitGraph.js
+++ src/components/home/OrbitGraph.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import * as d3 from 'd3';
-import styles from '../../styles/Techtree.module.css';
+import React from 'react'
+import * as d3 from 'd3'
+import styles from '../../styles/Techtree.module.css'
 
 export default function ForceGraph({ techtreeData, category }) {
-  const containerRef = React.useRef(null);
+  const containerRef = React.useRef(null)
 
   const nodeHoverTooltip = (node) => {
     return `<div>     
       <p><b>${node.name}</b></p>
       <p>최근 5년<br />마일리지 커트라인<br />${node.recentMileage}</p>
-    </div>`;
-  };
+    </div>`
+  }
 
   React.useEffect(() => {
-    let destroyFn;
+    let destroyFn
 
     if (containerRef.current) {
       const { destroy } = runForceGraph(
@@ -21,24 +21,24 @@ export default function ForceGraph({ techtreeData, category }) {
         techtreeData,
         category,
         nodeHoverTooltip
-      );
-      destroyFn = destroy;
+      )
+      destroyFn = destroy
     }
 
-    return destroyFn;
-  }, []);
+    return destroyFn
+  }, [])
 
-  return <div ref={containerRef} className={styles.container} />;
+  return <div ref={containerRef} className={styles.container} />
 }
 
 function runForceGraph(container, techtreeData, category, nodeHoverTooltip) {
   // linksData 대신, 객체 전체를 받아야지 이게 어느 과목인지도 확인할 수 있음.
 
-  //const orbitRadius1 = 0;
-  //const orbitRadius2 = 80;
-  //const orbitRadius3 = 160;
-  //const orbitRadius4 = 240;
-
+  const orbitRadius1 = 0
+  const orbitRadius2 = 120
+  const orbitRadius3 = 200
+  const orbitRadius4 = 280
+  const links = techtreeData.links.map((d) => Object.assign({}, d))
   const nodes = techtreeData.nodes.map((d) => {
     switch (d.group) {
       case 1:
@@ -46,33 +46,33 @@ function runForceGraph(container, techtreeData, category, nodeHoverTooltip) {
           ...d,
           r: 10,
           R: 0,
-          phi0: 0, //Math.ceil(Math.random() * 360),
+          phi0: Math.ceil(Math.random() * 360),
           speed: Math.random() * 5,
-        };
+        }
       case 2:
         return {
           ...d,
           r: 10,
-          R: 80,
-          phi0: 0, //Math.ceil(Math.random() * 360),
+          R: orbitRadius2,
+          phi0: Math.ceil(Math.random() * 360),
           speed: Math.random() * 5,
-        };
+        }
       case 3:
         return {
           ...d,
           r: 10,
-          R: 160,
-          phi0: 0, //Math.ceil(Math.random() * 360),
+          R: orbitRadius3,
+          phi0: Math.ceil(Math.random() * 360),
           speed: Math.random() * 5,
-        };
+        }
       case 4:
         return {
           ...d,
           r: 10,
-          R: 240,
-          phi0: 0, //Math.ceil(Math.random() * 360),
+          R: orbitRadius4,
+          phi0: Math.ceil(Math.random() * 360),
           speed: Math.random() * 5,
-        };
+        }
       default:
         return {
           ...d,
@@ -80,91 +80,125 @@ function runForceGraph(container, techtreeData, category, nodeHoverTooltip) {
           R: 300,
           phi0: 0, //Math.ceil(Math.random() * 360),
           speed: Math.random() * 5,
-        };
+        }
     }
-  });
+  })
 
   //const containerRect = container.getBoundingClientRect();
-  const height = 1000; //containerRect.height;
-  const width = 900; //containerRect.width;
-  const t0 = Date.now();
+  const height = 1000 //containerRect.height;
+  const width = 900 //containerRect.width;
+  const t0 = Date.now()
 
-  let xScale = d3.scaleLinear().domain([0, width]).range([0, width]);
-  let yScale = d3.scaleLinear().domain([0, height]).range([0, height]);
+  let xScale = d3.scaleLinear().domain([0, width]).range([0, width])
+  let yScale = d3.scaleLinear().domain([0, height]).range([0, height])
 
   const svg = d3
     .select(container)
     .append('svg')
-    .attr('viewBox', [-width / 2, -height / 3, width, height * 0.75]);
+    .attr('viewBox', [-width / 2, -height / 3, width, height * 0.75])
 
-  svg
-    .append('circle')
-    .attr('r', 80)
-    .attr('fill', 'none')
-    .attr('stroke', '#999');
-  svg
-    .append('circle')
-    .attr('r', 160)
-    .attr('fill', 'none')
-    .attr('stroke', '#999');
-  svg
-    .append('circle')
-    .attr('r', 240)
-    .attr('fill', 'none')
-    .attr('stroke', '#999');
+  const orbit = svg.append('g').attr('class', 'orbit')
+
+  orbit.append('circle').attr('r', orbitRadius2).attr('fill', 'none').attr('stroke', '#999')
+  orbit.append('circle').attr('r', orbitRadius3).attr('fill', 'none').attr('stroke', '#999')
+  orbit.append('circle').attr('r', orbitRadius4).attr('fill', 'none').attr('stroke', '#999')
 
   const node = svg
     .append('g')
-    .attr('stroke-width', 2)
+    //.attr('stroke-width', 2)
     .selectAll('circle')
     .data(nodes)
     .join('circle')
+
+  node
     .attr('r', (d) => {
-      return d.r;
+      return d.r
     })
     .attr('cx', (d) => {
-      return d.R;
+      return d.R
     })
     .attr('cy', (d) => {
-      return 0;
+      return 0
     })
     .attr('class', (d) => {
-      return d.name;
+      return `node${d.id}`
     })
+    .attr('fill', '#00bebe')
+    .attr('stroke', '#00bebe')
     .attr('transform', (d) => {
-      const delta = Date.now() - t0;
-      return `rotate(${d.phi0 + delta * d.speed * 0.002})`;
+      const delta = Date.now() - t0
+      return `rotate(${d.phi0 + delta * d.speed * 0.002})`
     })
+
+  node
     .on('mouseover', (d) => {
-      clearInterval(timeFlies);
+      clearInterval(timeFlies)
+      addTooltip(nodeHoverTooltip, d, d3.event.pageX, d3.event.pageY)
+      fadeExceptSelected(d)
+      node.style('cursor', 'pointer')
     })
     .on('mouseout', (d) => {
       timeFlies = setInterval(() => {
-        const delta = Date.now() - t0;
+        const delta = Date.now() - t0
         node.attr('transform', (d) => {
-          return `rotate(${d.phi0 + delta * d.speed * 0.002})`;
-        });
-      }, 40);
-    });
+          return `rotate(${d.phi0 + delta * d.speed * 0.002})`
+        })
+      }, 40)
+      tooltip.style('opacity', 0)
+      node.style('opacity', '1')
+      orbit.style('opacity', '1')
+    })
 
-  function tick() {
-    // update node positions
-    node.attr('cx', (d) => xScale(d.x)).attr('cy', (d) => yScale(d.y));
+  const tooltip = d3.select(container).append('div')
+
+  const addTooltip = (hoverTooltip, node, x, y) => {
+    tooltip
+      .html(hoverTooltip(node))
+      .attr('class', 'tooltip')
+      .style('left', `${x + 40}px`)
+      .style('top', `${y - 120}px`)
+      .style('opacity', 0.99)
+  }
+
+  function fadeExceptSelected(selectedNode) {
+    node.style('opacity', '0.2')
+    orbit.style('opacity', '0.5')
+
+    links.map((linkElement) => {
+      // linkElement 의 source, target은 node 객체 값이 들어가있음.
+      if (linkElement.source === selectedNode.id) {
+        // 여기서 target과 selected의 style만 바꿔주면 됨.
+        console.log(selectedNode)
+        svg.select(`circle.node${selectedNode.id}`).style('opacity', '1')
+        svg.select(`circle.node${linkElement.target}`).style('opacity', '1')
+        console.log('if!')
+        return
+      } else if (linkElement.target === selectedNode.id) {
+        console.log('else if!')
+        node.select(`node${selectedNode.id}`).style('opacity', '1')
+        svg.select(`circle.node${linkElement.source}`).style('opacity', '1')
+        return
+      } else {
+        console.log('else!')
+        node.select(`node${selectedNode.id}`).style('opacity', '1')
+        return
+      }
+    })
   }
 
   let timeFlies = setInterval(() => {
-    const delta = Date.now() - t0;
+    const delta = Date.now() - t0
     node.attr('transform', (d) => {
-      return `rotate(${d.phi0 + delta * d.speed * 0.002})`;
-    });
-  }, 40);
+      return `rotate(${d.phi0 + delta * d.speed * 0.002})`
+    })
+  }, 40)
 
   return {
     destroy: () => {
       //simulation.stop();
     },
     nodes: () => {
-      return svg.node();
+      return svg.node()
     },
-  };
+  }
 }
