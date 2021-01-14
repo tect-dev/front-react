874e9fde0c752073b40a2a47cb3049426183b912
zowan <zowan@zowanui-MacBookPro.local>
2021-01-15 01:04:52 +0900

    todo: 시간순 정렬 / 페이지네이션 /글수정 /로그인

--- src/pages/question/QuestionListPage.js
+++ src/pages/question/QuestionListPage.js
@@ -1,51 +1,51 @@
-import { useCallback, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import MainLayout from '../../components/layout/MainLayout';
-import { readQuestionList } from '../../redux/readPost';
-import { Link } from 'react-router-dom';
-import QuestionBlock from '../../components/question/QuestionBlock';
+import { useCallback, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import MainLayout from '../../components/layout/MainLayout'
+import { readQuestionList } from '../../redux/readPost'
+import { Link } from 'react-router-dom'
+import QuestionBlock from '../../components/question/QuestionBlock'
 
-import { Button } from '../../components/Button';
+import { Button } from '../../components/Button'
 
-import '../../styles/page/question/QuestionListPage.scss';
+import '../../styles/page/question/QuestionListPage.scss'
 
 export default function QuestionListPage() {
   const { loading, data, error } = useSelector((state) => {
-    return state.readPost.questionList;
-  });
+    return state.readPost.questionList
+  })
 
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
 
   // useCallback : 함수의 불필요한 리렌더링을 막기 위한 hooks.
   // react 는 컴포넌트가 리렌더링되면 함수도 새로 생기는데, 반복적으로 사용하는 함수를 리렌더링 하지 않고 재사용하기 위함.
   const getQuestionListAsync = useCallback(() => {
-    dispatch(readQuestionList());
-  }, [dispatch]);
+    dispatch(readQuestionList())
+  }, [dispatch])
 
   useEffect(async () => {
-    getQuestionListAsync();
-    console.log(data);
-  }, [dispatch]);
+    getQuestionListAsync()
+    console.log(data)
+  }, [dispatch])
 
   if (loading)
     return (
       <MainLayout>
         <div>로딩중...</div>
       </MainLayout>
-    );
+    )
   if (error)
     return (
       <MainLayout>
         <div>error...</div>
       </MainLayout>
-    );
+    )
 
   if (!data)
     return (
       <MainLayout>
         <div>no data</div>
       </MainLayout>
-    );
+    )
 
   return (
     <>
@@ -68,7 +68,11 @@ export default function QuestionListPage() {
               <div className="questionList">
                 {data
                   ? data.map((element, index) => {
-                      return <QuestionBlock key={index} question={element} />;
+                      return (
+                        <div key={index}>
+                          <QuestionBlock question={element} />
+                        </div>
+                      )
                     })
                   : ''}
               </div>
@@ -82,5 +86,5 @@ export default function QuestionListPage() {
         </div>
       </MainLayout>
     </>
-  );
+  )
 }
