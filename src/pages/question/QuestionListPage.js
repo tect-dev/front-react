82e0b903abe0f666693f8fb0df4ebc6d7215282a
zowan <zowan@zowanui-MacBookPro.local>
2021-01-17 01:31:23 +0900

    done: 로그인

--- src/pages/question/QuestionListPage.js
+++ src/pages/question/QuestionListPage.js
@@ -1,4 +1,4 @@
-import { useCallback, useEffect } from 'react'
+import { useCallback, useEffect, useLayoutEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import MainLayout from '../../components/layout/MainLayout'
 import { readQuestionList } from '../../redux/readPost'
@@ -20,7 +20,7 @@ export default function QuestionListPage() {
   // useCallback : 함수의 불필요한 리렌더링을 막기 위한 hooks.
   // react 는 컴포넌트가 리렌더링되면 함수도 새로 생기는데, 반복적으로 사용하는 함수를 리렌더링 하지 않고 재사용하기 위함.
 
-  useEffect(async () => {
+  useEffect(() => {
     dispatch(readQuestionList())
   }, [dispatch])
 
