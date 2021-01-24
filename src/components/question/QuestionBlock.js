bae291c4b348290c8f787e6cef534cf21c6a3f04
황지섭 <ghkdwltjq98@naver.com>
2021-01-24 16:40:31 +0900

    API 연동 시도(미완성)
    question 작성하고 백엔드 서버에 전송 후 detail 페이지로 history.push를 하는 과정에서 서버에서 받은 데이터에 quetionAuthor가 담겨 있지 않아서 렌더링 에러가 발생함.

--- src/components/question/QuestionBlock.js
+++ src/components/question/QuestionBlock.js
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 import '../../styles/question/QuestionBlock.scss'
 
 export default function QuestionBlock({ question }) {
-  const summaryContent = question.questionBody.content.substr(0, 200)
+  const summaryContent = question.contentSubstring.substr(0, 200)
   //console.log(summaryContent);
   return (
     <Link className="questionBlock" to={`/question/${question._id}`}>
@@ -10,19 +10,19 @@ export default function QuestionBlock({ question }) {
         {/* 1000만 미만의 포인트에 대해서는 PC 버전이 깨지지 않음.✭ */}
         {/*<div className="questionBlock-points"> ✭ 1,000,000</div>*/}
         <div className="answerNum-container">
-          {question?.answerList?.length | 0}
+          {question.answerSum ? question?.answerSum : 0}
         </div>
         <div>Answers</div>
       </div>
       <div className="questionBlock-main">
         <div className="questionBlock-main-upside">
           <div className="questionBlock-title">
-            {question.questionBody.title}
+            {question.title}
           </div>
           <div className="questionBlock-content">{summaryContent}...</div>
         </div>
         <div className="questionBlock-main-downside">
-          {question.questionBody.hashtags.map((element, index) => {
+          {question?.hashtags?.map((element, index) => {
             return (
               <div className="questionBlock-hashtag" key={index}>
                 {element}
