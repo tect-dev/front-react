8a7d0f7e98daed3f398a59d5444596bef3ba5261
budlebee <bbogle7613@gmail.com>
2021-01-24 16:40:31 +0900

    API 연동 시도(미완성)
    question 작성하고 백엔드 서버에 전송 후 detail 페이지로 history.push를 하는 과정에서 서버에서 받은 데이터에 quetionAuthor가 담겨 있지 않아서 렌더링 에러가 발생함.

--- src/components/question/QuestionSection.js
+++ src/components/question/QuestionSection.js
@@ -11,51 +11,30 @@ import { deleteQuestion, deleteComment } from '../../redux/deletePost'
 import styled from 'styled-components'
 
 export default React.memo(function QuestionSection({ data }) {
-  const [content, setContent] = useState('')
+  const [ question, setQuestion ] = useState(data.questionList[0])
   const { userID } = useSelector((state) => {
     return { userID: state.auth.userID }
   })
-
-  function onChangeContent(value) {
-    setContent(value)
-  }
 
   const dispatch = useDispatch()
 
   const onDeleteQuestion = useCallback(() => {
     //alert('정말 삭제합니까?');
-    dispatch(deleteQuestion(data.question._id))
+    dispatch(deleteQuestion(question._id))
   }, [dispatch])
 
   function deleteComment() {
     alert('정말 삭제합니까?')
   }
 
-  const onSubmitComment = useCallback(
-    async (e) => {
-      e.preventDefault()
-      if (!content) {
-        return
-      }
-      const formData = new FormData()
-      const uid24 = uid(24)
-      formData.append('postID', uid24)
-      formData.append('contentType', 'question')
-      formData.append('content', content)
-      formData.append('authorID', '123456789012345678901234')
-      formData.append('authorNickname', '임시닉네임')
-    },
-    [content]
-  )
-
   return (
     <QuestionContainer>
       <div className="title">
-        <h2>Q. {data.question.questionBody.title}</h2>
+        <h2>Q. {question.title}</h2>
         <br />
       </div>
       <div className="hashtags">
-        {data.question.questionBody.hashtags.map((tag, index) => {
+        {question?.hashtags?.map((tag, index) => {
           return (
             <TagBlock
               key={index}
@@ -70,27 +49,31 @@ export default React.memo(function QuestionSection({ data }) {
         <br />
       </div>
       <div className="content">
-        <MarkdownRenderingBlock content={data.question.questionBody.content} />
+        <MarkdownRenderingBlock content={question.content} />
         <br />
         <br />
       </div>
 
       <div>
-        {data.question.updatedAt.substr(0, 4)}년{' '}
-        {data.question.updatedAt.substr(5, 2)}월{' '}
-        {data.question.updatedAt.substr(8, 2)}일
+        {question.updatedAt.substr(0, 4)}년{' '}
+        {question.updatedAt.substr(5, 2)}월{' '}
+        {question.updatedAt.substr(8, 2)}일
       </div>
       <div>
-        <Link to={`/user/${data.question.questionBody.authorID}`}>
-          {data.question.questionBody.authorNickname}
-        </Link>
+        {console.log(question)}
+        {question.questionAuthor.length === 0
+          ? null
+          : <Link to={`/user/${question?.questionAuthor[0]?._id}`}>
+              {question.questionAuthor[0]?.nickname}
+            </Link>
+        }
       </div>
-      {data.question.questionBody.authorID === userID &&
+      {question.questionAuthor[0]._id === userID &&
       data.answers.length === 0 &&
       userID !== '000000000000000000000000' ? (
         <>
           <Button>
-            <Link to={`/question/edit/${data.question._id}`}>
+            <Link to={`/question/edit/${question._id}`}>
               질문 수정하기
             </Link>
           </Button>
@@ -99,13 +82,6 @@ export default React.memo(function QuestionSection({ data }) {
       ) : (
         ''
       )}
-
-      {/*<CommentListBlock commentList={question.comments} />
-      <MarkdownEditorBlock
-        initialContent={''}
-        onChangeContentProps={onChangeContent}
-      />
-      <Button onClick={onSubmitComment}>question 에 댓글달기</Button>*/}
     </QuestionContainer>
   )
 })
