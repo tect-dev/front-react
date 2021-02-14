62580e10656c44e0a09d4d8e8974696137f4c8b5
budlebee <budlebeee@gmail.com>
2021-02-14 11:05:44 +0900

    done: 썸네일 만드는 로직 추가

--- src/wrappers/MainWrapper.js
+++ src/wrappers/MainWrapper.js
@@ -1,5 +1,7 @@
 import styled from 'styled-components'
 import Navbar from '../components/layout/Navbar'
+import Fab from '@material-ui/core/Fab'
+import { Container, Button, Link } from 'react-floating-action-button'
 
 import { colorPalette } from '../lib/constants'
 
@@ -8,6 +10,21 @@ export default function MainWrapperExport({ children }) {
     <MainLayout>
       <Navbar />
       <ContentWrapper>{children}</ContentWrapper>
+      <Container>
+        <Link href="/" tooltip="나무 심기" icon="fa fa-sticky-note" />
+        <Link
+          href="/"
+          tooltip="뭘 넣는게 좋을까 여기엔"
+          icon="fa fa-user-plus"
+        />
+
+        <Link
+          tooltip="내 숲으로 가기"
+          icon="fa fa-plus"
+          rotate={true}
+          href="/"
+        />
+      </Container>
     </MainLayout>
   )
 }
