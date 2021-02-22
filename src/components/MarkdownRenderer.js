96dee6ca87ad7008fed55436bc8e850204c0bb5e
budlebee <bbogle7613@gmail.com>
2021-02-22 21:18:46 +0900

    done: 마크다운 렌더러 스크롤 자동으로 내려가는것 추가

--- src/components/MarkdownRenderer.js
+++ src/components/MarkdownRenderer.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import styled from 'styled-components'
 import remark from 'remark'
 import remarkParse from 'remark-parse'
